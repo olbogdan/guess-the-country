7280dcf01d8198cc344d807212c2a4cf1ca4aa93
bogdanov <iceposhta@gmail.com>
2021-05-06 22:05:07 +0100

    show an alert with a selection result

--- guess the country/ContentView.swift
+++ guess the country/ContentView.swift
@@ -8,17 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let countries = ["UK", "USA", "Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Russia", "Sweden"].shuffled()
+    @State private var countries = ["UK", "USA", "Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Russia", "Sweden"].shuffled()
 
-    private var correctAnswer = Int.random(in: 0 ... 2)
+    @State private var correctAnswer = Int.random(in: 0 ... 2)
 
-    private var score = 0
+    @State private var score = 0
+
+    @State private var showingScore = false
+
+    @State private var scoreTitle = ""
 
     var body: some View {
         ZStack {
             LinearGradient(
-                gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
+                gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
                 VStack {
                     Text("Select flag:")
@@ -31,7 +36,7 @@ struct ContentView: View {
                 ForEach(0 ..< 3) { number in
                     Button(
                         action: {
-                            //
+                            flagTapped(number)
                         },
                         label: {
                             Image(self.countries[number])
@@ -41,7 +46,8 @@ struct ContentView: View {
                                 )
                                 .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 1))
                                 .shadow(color: .black, radius: 2)
-                        })
+                        }
+                    )
                 }
 
                 Text("Score: \(score)")
@@ -51,6 +57,31 @@ struct ContentView: View {
                 Spacer()
             }
         }
+        .alert(isPresented: $showingScore) {
+            Alert(
+                title: Text(scoreTitle),
+                message: Text("Score: \(score)"),
+                dismissButton: .default(Text("Continue")) {
+                    askQuestion()
+                }
+            )
+        }
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0 ... 2)
+    }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Right!"
+            score += 1
+        } else {
+            scoreTitle = "False :( It was \(countries[number])"
+            score -= 1
+        }
+        showingScore = true
     }
 }
 
