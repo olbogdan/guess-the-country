665d44d6fb86cbcd523849836841d6025c9fa89e
bogdanov <iceposhta@gmail.com>
2021-05-06 15:27:36 +0100

    Initial Commit

---
+++ guess the country/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guess the country
+//
+//  Created by bogdanov on 06.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ guess the country/guess_the_countryApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_countryApp.swift
+//  guess the country
+//
+//  Created by bogdanov on 06.05.21.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_countryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
