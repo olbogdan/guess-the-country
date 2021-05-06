acc4c92c643832ec115522f4a9460304551a35ed
bogdanov <iceposhta@gmail.com>
2021-05-06 20:22:59 +0100

    show country 3 random county flags

--- guess the country/ContentView.swift
+++ guess the country/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let countries = ["UK", "USA", "Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Russia", "Sweden"].shuffled()
+
+    private var correctAnswer = Int.random(in: 0 ... 2)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack {
+                    Text("Select flag:")
+                    Text(countries[correctAnswer])
+                        .fontWeight(.black)
+                }.foregroundColor(.white)
+                    .font(.largeTitle)
+
+                ForEach(0 ..< 3) { number in
+                    Button(action: {}, label: {
+                        Image(self.countries[number])
+                            .frame(width: 250, height: 130)
+                    })
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
